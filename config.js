c29bd80658cfe4a54dffa63c0aba5b09260d65e2
Ignis DayZ <68923566+holybdo@users.noreply.github.com>
2021-08-09 05:33:00 +0300

    map 1.0

---
+++ config.js
@@ -0,0 +1,52 @@
+var CONFIG = {
+  "maps": {
+    "map_myworld": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1628474367,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 8,
+      "name": "Dragon Magic",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "server_isometric_t1",
+      "tileSize": 384,
+      "world": "server",
+      "worldName": "server",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "map_myworld"
+  ],
+  "tileSetGroups": {
+    "server_isometric_t1": {
+      "maxZoom": 8,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
